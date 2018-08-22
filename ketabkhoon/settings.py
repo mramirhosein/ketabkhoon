1c044e6880ad15e68d5e30345c6935e20aa988ed
mramirhosein <42413830+mramirhosein@users.noreply.github.com>
2018-08-22 04:42:02 -0700

    Update settings.py

--- ketabkhoon/settings.py
+++ ketabkhoon/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8+j&jnqbd3oxhqzg8sb%3)2jppo%2a5=uq^r-2*1!&=wfv)0ao'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,18 +72,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'ketabkhoon.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ketabkhoon',
-        'PASSWORD':'amirhoseinroshan',
-        'USER':'ketabkhoon',
-        'PORT':5432,
-    }
-}
 
 
 # Password validation
@@ -120,13 +107,3 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
-
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS =(
-    os.path.join(BASE_DIR,'media/static/'),
-)
-
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
